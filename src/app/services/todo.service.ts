41d6bb54c2ffc6eeb143bd798094c200beec87b3
AlexMuia31 <alexmuia31@gmail.com>
2020-11-24 11:11:02 +0300

    toggle

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -3,6 +3,14 @@ import { from, Observable } from 'rxjs';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Todo } from '../models/Todo';
 
+
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,8 +19,14 @@ export class TodoService {
   todoLimit = '?_limit=10';
 
   constructor(private http:HttpClient) { }
+  //Get Todos
   getTodos():Observable<Todo[]>{
     return this.http.get<Todo[]>(`${this.todoUrl}${this.todoLimit}`);
     
+  }
+  // Toggle Completed
+  toggleCompleted(todo: Todo): Observable<any>{
+    const url = `${this.todoUrl}/${todo.id}`;
+    return this.http.put(url, todo, httpOptions);
   }
 }
